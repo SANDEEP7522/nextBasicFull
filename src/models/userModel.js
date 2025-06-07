232e0b1e52e5f2aade326d63dae72e41f8a16a73
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-06-07 17:53:34 +0530

    Added userSchema

---
+++ src/models/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+     username: {
+          type: String,
+          required: [true, "Please add a username"],
+          unique: true,
+     },
+     email: {
+          type: String,
+          required: [true, "Please add an email"],
+          unique: true,
+     },
+     password: {
+          type: String,
+          required: [true, "provide a password"],
+     },
+     isVerified: {
+          type: Boolean,
+          required: true,
+          default: false,
+     },
+     isAdmin: {
+          type: Boolean,
+          required: true,
+          default: false,
+     },
+     forgetPasswordToken: String,
+     forgetPasswordTokenExpiry: Date,
+     verifyToken: String,
+     verifyTokenExpiry: Date
+});
+
+ const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
